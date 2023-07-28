58fa590e0f8e026c59816faec714a844fcd26dff
‘liuning’ <liuning@zywljs.com>
2023-07-28 19:27:11 +0800

    Revert "initial commit"
    This reverts commit 3a930c939616c59400247045b9906430db3cafc2.

--- src/main/java/com/example/mapper/AccoutMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.example.pojo.Accout;
-import org.apache.ibatis.annotations.Select;
-
-public interface AccoutMapper {
-    @Select("select * from user where `userName` = #{name}")
-    Accout GetUserByName(String name);
-}
