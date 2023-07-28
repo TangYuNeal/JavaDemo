c9b3f12b9c8007dab6cbfef87441e5b9aa13163c
‘liuning’ <liuning@zywljs.com>
2023-07-28 19:56:29 +0800

    add FindAccount

--- src/main/java/com/example/controller/AccoutController.java
+++ src/main/java/com/example/controller/AccoutController.java
@@ -1,7 +1,16 @@
 package com.example.controller;
 
+import com.example.pojo.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AccoutController {
+    @GetMapping
+    public String FindAccount() {
+        return "Accouts";
+    }
+
 }
